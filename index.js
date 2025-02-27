c4b016379597869e3a8e118c303f82e92647fb0b
EZRA KIPRUTO SEREM <ezraserem4038@gmail.com>
2025-02-27 22:20:45 +0300

    functions

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+// Define employee object
+const employee = {
+    name: 'Sam'
+  };
+  
+  // Non-destructive function to update employee with key and value
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+  }
+  
+  // Destructive function to update employee with key and value
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  // Non-destructive function to delete key from employee
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  // Destructive function to delete key from employee
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
